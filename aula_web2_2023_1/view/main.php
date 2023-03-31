c1afa7b957086c40ac64e0f3b677ccd6312996d4
Manuvoc <128625827+Manuvoc@users.noreply.github.com>
2023-03-31 15:26:58 -0300

    Add files via upload

---
+++ aula_web2_2023_1/view/main.php
@@ -0,0 +1,19 @@
+<?php
+
+include '../Util.php';
+//Util::verificar();
+
+	//session_start();
+?>
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+		<h3>Sistema Academico</h3>
+		<?php
+			//	echo "Seja bem vindo usuário: " .$_SESSION['login']. "<a href='login.php'>Sair</a>";
+		?>
+    <a href="UsuarioList.php">Usuario</a>
+  </body>
+</html>
